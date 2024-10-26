65fee6620ef38fe2c0e7484ff945eb52b3a6b29a
oliverrich <oliverrichards12@gmail.com>
2024-10-25 20:19:10 -0700

    Add files via upload

---
+++ utils/customer.h
@@ -0,0 +1,19 @@
+//
+// Created by Oliver on 10/25/2024.
+//
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+
+#include <string>
+using namespace std;
+
+struct Customer {
+    int id;
+    string name;
+    string email;
+};
+
+#endif // CUSTOMER_H
+
